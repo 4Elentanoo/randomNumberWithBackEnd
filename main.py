4df5cfa7970e1fea6ef463b22ee8b26c92f38ca6
Gareynov Grigoriy <g.gareinov@fact.digital>
2025-11-04 20:54:29 +0500

    cgi

--- main.py
+++ main.py
@@ -1,12 +1,67 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, session, make_response, request
+import random as rnd
+import math
 
 app = Flask(__name__)
+# Сделать хранение данных в сессии
+
+start_range = 1
+end_range = 120
+count = math.ceil(math.log2(end_range - start_range + 1))
+maxCount = count
+rnd_num = rnd.randint(start_range, end_range)
+history_numbers = []
 
 
 @app.route("/")
-def hello():
-    return render_template("base.html")
+def main():
+    global count, start_range, end_range, rnd_num, history_numbers, maxCount
+    return render_template("start_game.html", start_range=start_range, end_range=end_range, count=count, history_numbers=history_numbers, rnd_num=rnd_num, maxCount=maxCount, error="")
+
+
+@app.route("/check", methods=['POST'])
+def check():
+    # брать переменную из сессии
+    global count, rnd_num, history_numbers
+    count -= 1
+    number = request.form['enter__number']
+    if count == 0:
+        history_numbers = []
+        return end_game()
+    try:
+        if number != "":
+            history_numbers.append(int(number))
+            if int(number) == rnd_num:
+                history_numbers = []
+                return win_game()
+        elif request.form['new_game'] == "" and number == "":
+            return new_game()
+    except:
+        return main()
+    return main()
+
+
+@app.route("/end_game", methods=['GET'])
+def end_game():
+    global rnd_num
+    return render_template("end_game.html", rnd_num=rnd_num)
+
+
+@app.route("/new_game", methods=['GET'])
+def new_game():
+    global count, start_range, end_range, rnd_num, history_numbers
+    count = math.ceil(math.log2(end_range - start_range + 1))
+    rnd_num = rnd.randint(start_range, end_range)
+    history_numbers = []
+    print(rnd_num)
+    return main()
+
+
+@app.route("/win_game",)
+def win_game():
+    global rnd_num
+    return render_template("win_game.html", rnd_num=rnd_num)
 
 
 if __name__ == "__main__":
--- static/js/main.js
+++ static/js/main.js
@@ -1,121 +1,120 @@
 "use strict";
-const startRange = 100;
-const endRange = 140;
-let historyNumber = [];
-let randomNumber = Math.round(
-  startRange + Math.random() * (endRange - startRange)
-);
-let maxAttempts = Math.ceil(Math.log2(endRange - startRange + 1));
-let attempts = Math.ceil(Math.log2(endRange - startRange + 1));
-const startRangeElement = document.querySelector(".start__range");
-startRangeElement.innerHTML = startRange;
-const endRangeElement = document.querySelector(".end__range");
-endRangeElement.innerHTML = endRange;
-const getNumberInput = document.querySelector(".enter__number");
-const getCheckNumberBtn = document.querySelector(".check__btn");
-const getHistoryBlock = document.querySelector(".history__list");
-const getNewNumberBtn = document.querySelector(".new__number__btn");
+console.log("HELLO");
+// const startRange = 100;
+// const endRange = 140;
+// let historyNumber = [];
+// let randomNumber = Math.round(
+//   startRange + Math.random() * (endRange - startRange)
+// );
+// let maxAttempts = Math.ceil(Math.log2(endRange - startRange + 1));
+// let attempts = Math.ceil(Math.log2(endRange - startRange + 1));
+// const getNumberInput = document.querySelector(".enter__number");
+// const getCheckNumberBtn = document.querySelector(".check__btn");
+// const getHistoryBlock = document.querySelector(".history__list");
+// const getNewNumberBtn = document.querySelector(".new__number__btn");
 const progressBar = document.querySelector(".progress-bar");
-const mainDashboard = document.querySelector(".table");
-const restartDashboard = document.querySelector(".table__result");
-const resultMessage = document.querySelector(".result__subtitle");
-const restartGameBtn = document.querySelector(".restart__btn");
+// const mainDashboard = document.querySelector(".table");
+// const restartDashboard = document.querySelector(".table__result");
+// const resultMessage = document.querySelector(".result__subtitle");
+// const restartGameBtn = document.querySelector(".restart__btn");
 const bodyElement = document.querySelector("body");
-const errorMsg = document.querySelector(".error__msg");
+const attempts = document.querySelector(".start_range");
+const maxAttempts = document.querySelector(".end_range");
+progressBar.style.width = `${((+attempts.innerHTML) / (+maxAttempts.innerHTML)) * 100}%`;
+// console.log(randomNumber);
+// console.log(`Tries: ${attempts}`);
+// function checkNumber() {
+//   if (
+//     getNumberInput.value &&
+//     +getNumberInput.value >= startRange &&
+//     +getNumberInput.value <= endRange
+//   ) {
+//     --attempts;
+//     historyNumber.push(
+//       `<span>${+getNumberInput.value < randomNumber ? ">" : "<"}${
+//         getNumberInput.value
+//       }</span>`
+//     );
+//     getHistoryBlock.innerHTML = historyNumber.toString();
+// progressBar.style.width = `${(attempts / maxAttempts) * 100}%`;
+//     if (randomNumber == +getNumberInput.value || attempts == 0) {
+//       mainDashboard.style.display = "none";
+//       restartDashboard.style.display = "block";
+//       resultMessage.innerHTML = `Вы ${
+//         randomNumber == +getNumberInput.value ? "победили" : "проиграли"
+//       }, загаданное число было: ${randomNumber}`;
+//       restartGameBtn.focus();
+//     }
+//     getNumberInput.value = "";
+//     getNumberInput.focus();
+//     errorMsg.style.display = "none";
+//   } else {
+//     errorMsg.style.display = "block";
+//   }
+// }
 
-console.log(randomNumber);
-console.log(`Tries: ${attempts}`);
-function checkNumber() {
-  if (
-    getNumberInput.value &&
-    +getNumberInput.value >= startRange &&
-    +getNumberInput.value <= endRange
-  ) {
-    --attempts;
-    historyNumber.push(
-      `<span>${+getNumberInput.value < randomNumber ? ">" : "<"}${
-        getNumberInput.value
-      }</span>`
-    );
-    getHistoryBlock.innerHTML = historyNumber.toString();
-    progressBar.style.width = `${(attempts / maxAttempts) * 100}%`;
-    if (randomNumber == +getNumberInput.value || attempts == 0) {
-      mainDashboard.style.display = "none";
-      restartDashboard.style.display = "block";
-      resultMessage.innerHTML = `Вы ${
-        randomNumber == +getNumberInput.value ? "победили" : "проиграли"
-      }, загаданное число было: ${randomNumber}`;
-      restartGameBtn.focus();
-    }
-    getNumberInput.value = "";
-    getNumberInput.focus();
-    errorMsg.style.display = "none";
-  } else {
-    errorMsg.style.display = "block";
-  }
-}
+// function restartGame() {
+//   historyNumber = [];
+//   errorMsg.style.display = "none";
+//   getNumberInput.value = "";
+//   getHistoryBlock.innerHTML = "";
+//   randomNumber = Math.round(
+//     startRange + Math.random() * (endRange - startRange)
+//   );
+//   attempts = Math.log2(endRange - startRange + 1).toFixed();
+//   progressBar.style.width = "100%";
+//   mainDashboard.style.display = "block";
+//   restartDashboard.style.display = "none";
+//   getNumberInput.focus();
+// }
 
-function restartGame() {
-  historyNumber = [];
-  errorMsg.style.display = "none";
-  getNumberInput.value = "";
-  getHistoryBlock.innerHTML = "";
-  randomNumber = Math.round(
-    startRange + Math.random() * (endRange - startRange)
-  );
-  attempts = Math.log2(endRange - startRange + 1).toFixed();
-  progressBar.style.width = "100%";
-  mainDashboard.style.display = "block";
-  restartDashboard.style.display = "none";
-  getNumberInput.focus();
-}
+// function newNumber() {
+//   historyNumber = [];
+//   getNumberInput.value = "";
+//   getHistoryBlock.innerHTML = "";
+//   progressBar.style.width = "100%";
+//   randomNumber = +(Math.random() * 100).toFixed();
+//   console.log(randomNumber);
+//   getNumberInput.focus();
+//   attempts = Math.log2(endRange - startRange + 1).toFixed();
+// }
 
-function newNumber() {
-  historyNumber = [];
-  getNumberInput.value = "";
-  getHistoryBlock.innerHTML = "";
-  progressBar.style.width = "100%";
-  randomNumber = +(Math.random() * 100).toFixed();
-  console.log(randomNumber);
-  getNumberInput.focus();
-  attempts = Math.log2(endRange - startRange + 1).toFixed();
-}
+// getCheckNumberBtn.addEventListener("click", () => {
+//   checkNumber();
+// });
 
-getCheckNumberBtn.addEventListener("click", () => {
-  checkNumber();
-});
+// getCheckNumberBtn.addEventListener("keyup", (event) => {
+//   if (event.code === "Enter") {
+//     checkNumber();
+//   }
+// });
 
-getCheckNumberBtn.addEventListener("keyup", (event) => {
-  if (event.code === "Enter") {
-    checkNumber();
-  }
-});
+// getNewNumberBtn.addEventListener("keyup", (event) => {
+//   if (event.code === "Enter") {
+//     newNumber();
+//   }
+// });
 
-getNewNumberBtn.addEventListener("keyup", (event) => {
-  if (event.code === "Enter") {
-    newNumber();
-  }
-});
+// getNewNumberBtn.addEventListener("click", () => {
+//   newNumber();
+// });
 
-getNewNumberBtn.addEventListener("click", () => {
-  newNumber();
-});
-
-restartGameBtn.addEventListener("click", () => restartGame());
-restartGameBtn.addEventListener("keyup", (event) => {
-  if (event.code === "Enter") {
-    restartGame();
-  }
-});
+// restartGameBtn.addEventListener("click", () => restartGame());
+// restartGameBtn.addEventListener("keyup", (event) => {
+//   if (event.code === "Enter") {
+//     restartGame();
+//   }
+// });
 
 bodyElement.addEventListener("keyup", (event) => {
   if (event.code === "Escape") {
-    restartGame();
+    // restartGame();
+    location.reload();
   }
 });
 
-getNumberInput.addEventListener("keyup", (event) => {
-  if (event.code === "Enter") {
-    checkNumber();
-  }
-});
+// getNumberInput.addEventListener("keyup", (event) => {
+//   if (event.code === "Enter") {
+//     checkNumber();
+//   }
+// });
